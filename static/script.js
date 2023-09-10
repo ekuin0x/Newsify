7b4b54d66bfc69680aadf6433794b4067271b01a
ekuin0x <salhimouiz1@gmaiL.com>
2023-09-10 21:38:20 +0100

    Fix google analytics

--- static/script.js
+++ static/script.js
@@ -1,7 +1,12 @@
 (async()=>{
-    const res0 = await fetch('/head')
-    const head = await res0.text()
-    $("head").append(head) 
+    let head = 
+    "<script async src='https://www.googletagmanager.com/gtag/js?id=G-KBVD10W104'></script><script>"+
+    "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}"+
+    "gtag('js', new Date());gtag('config', 'G-KBVD10W104');</script>"
+    $("head").prepend(head)
+    //const res0 = await fetch('/head')
+    //const head = await res0.text()
+    //$("head").append(head) 
 
     const q = $("#q").attr("name")
     url = `/related/${q}`
