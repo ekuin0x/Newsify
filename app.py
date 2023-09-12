35320ebe3a04b8364ed94e69378da476ca5fbf7d
ekuin0x <salhimouiz1@gmaiL.com>
2023-09-12 13:29:13 +0100

    lqjdgnf

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from flask import Flask , render_template, url_for, request, send_file
 import schedule as sc
 import requests
 import time
-
-def wake() : 
-    res1 = requests.get("https://newzone.onrender.com")
-    res2 = requests.get("https://convertly.onrender.com")
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -42,8 +37,3 @@ def articles(i) :
 if __name__ == "__main__" :
     app.run()
 
-wake()
-sc.every(10).minutes.do(wake)
-while True:
-    sc.run_pending()
-    time.sleep(1)
--- wake.py
+++ wake.py
@@ -4,7 +4,6 @@ import time
 
 def wake() : 
     res1 = requests.get("https://newzone.onrender.com")
-    res2 = requests.get("https://convertly.onrender.com")
 
 wake()
 sc.every(10).minutes.do(wake)
