23057d0a5d8fd5bb45226f3f270a2fa31beed597
ekuin0x <salhimouiz1@gmaiL.com>
2023-09-13 13:10:29 +0100

    lqjdgn

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import requests
 import time
 import random
 
-topics = [ "Global", "Sport","Politics", "India", "latest", "Health", "Technology"]
-key = "pub_265336ee2696ca69ba98f092f4122499822c6"
-
+topics = [ "Global", "Sport","Politics", "India", "Latest", "Health", "Technology"]
+#key = "pub_265336ee2696ca69ba98f092f4122499822c6"
+key = "pub_277439a73bbbb0d3aa7c80b32ebc7e246d8d4"
 app = Flask(__name__)
 
 @app.route('/')
