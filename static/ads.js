66684f029deacb7e9b9ad7921e6ed424e834db57
ekuin0x <salhimouiz1@gmaiL.com>
2023-09-14 12:24:53 +0100

    hhhh

--- static/ads.js
+++ static/ads.js
@@ -1,10 +1,11 @@
+/*
 (function(d,z,s){s.src='https://'+d+'/400/'+z;
 try{
     (document.body||document.documentElement).appendChild(s)}catch(e){}
 })('mutcheng.net',6308070,document.createElement('script'))
-
+*/
 $(document).ready(()=>{
-    $("#mainad a").attr("href", "//meenetiy.com/4/6308318") 
+    $("#mainad a").attr("href", "//greewepi.net/4/6329917") 
     $("#mainad a").attr("target", "_blank") 
     $("#mainad img").attr("src", "../static/media/mainad.png") 
     $("head").append("<meta name='monetag' content='2866a215c89832e0e8a0de4b6749b6e5'>")
@@ -18,7 +19,7 @@ setTimeout(() => {
 $("body").click(()=>{
     if (ad == 0 ){  
         ad = 1
-        window.open('//meenetiy.com/4/6308318', '_blank');
+        window.open('//greewepi.net/4/6329917', '_blank');
         setTimeout(()=>{
             ad = 0
         }, 8000)
