3aa79a3c5f90c6d0ab569d6dbcace2d3240c9356
ekuin0x <salhimouiz1@gmaiL.com>
2023-09-15 10:36:51 +0100

    Suuup

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask , render_template, url_for, request, send_file
 from jinja2 import Template
-import requests
 import time
 import random
 
--- static/script.js
+++ static/script.js
@@ -10,4 +10,12 @@
 $(document).ready(()=>{
     $("form").attr("action", "javascript:void(0);")
     $("button").attr("type", "submit") 
+    $("button").attr("onclick", "search()") 
 })
+
+const search = async ()=>{
+    let q = $("input[type='search']").val()
+    let res = await fetch("/search/" + q)
+    let html = await res.text()
+    $("#wrapper").html(html)
+}
